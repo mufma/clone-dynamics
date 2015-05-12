f03149cda7af9be164ac2bdb1b58cb8e46ab0d24
mufma <mufma@mail.ru>
2015-05-12 15:43:55 -0400

    Added extraction of data from simulation

--- corebeta.py
+++ corebeta.py
@@ -42,7 +42,7 @@ class colony:
 
 class reaction:
 
-	def __init__(self, reactants, products, name, colonies):
+	def __init__(self, reactants, products, name, colonies, rate):
 		"""
 		Description of object in class reaction. 
 
@@ -51,6 +51,7 @@ class reaction:
 		products - dictionary
 		name - string
 		colonies - list of colony objects
+		rate - float
 
 		Comments:
 		Keys of the dictionaries rectants and products should be named accoring to colonies names.
@@ -60,18 +61,19 @@ class reaction:
 		Reaction of type X + X -> Y can be written as
 		>> X = colony('Xome',24)
 		>> Y = colony('Yome',56)
-		>> R = reaction({'Xome': 2},{'Yome': 1}, 'conversion', [X, Y])
+		>> R = reaction({'Xome': 2},{'Yome': 1}, 'conversion', [X, Y], 0.5)
 		"""
 		self.name = name
 		self.reactants = reactants
 		self.products = products
 		self.colonies = colonies
+		self.rate = rate
 
 class gillespie:
 	"""
 	Algorithm of a stochastic simulation.
 	"""
-	def __init__(self, reactions):
+	def __init__(self, reactions, colonies):
 		"""
 		Description of object in class gillespie.
 
@@ -79,11 +81,12 @@ class gillespie:
 		reactions - list of reaction objects
 
 		Usage example:
-		>> R1 = reaction({'X': 2},{'Y': 1}, 'X conversion', [X, Y])
-		>> R2 = reaction({'Z': 3},{'Y': 4}, 'Z conversion', [X, Y])
-		>> G = gillespie([R1, R2])
+		>> R1 = reaction({'X': 2},{'Y': 1}, 'X conversion', [X, Y], 0.5)
+		>> R2 = reaction({'Z': 3},{'Y': 4}, 'Z conversion', [X, Y], 0.3)
+		>> G = gillespie([R1, R2], [X, Y, Z])
 		"""
 		self.reactions = reactions
+		self.colonies = colonies
 
 	def probability(self):
 		"""
@@ -96,9 +99,11 @@ class gillespie:
 				name = colony.name
 				try:
 					num = reaction.reactants[name]
-					prob = choose(num, colony.size)
+					prob = choose(colony.size, num)*reaction.rate
 					a.append(prob)
 					a0 += prob
+				except:
+					pass
 		return a0, a
 
 	def choosereaction(self, a0, a):
@@ -124,20 +129,27 @@ class gillespie:
 
 	def run(self, steps):
 		"""
+		Perform stochstic simulation
 		"""
 		data = {}
+		for colony in self.colonies:
+			data[colony.name] = []
+			data[colony.name].append(colony.size)
 
 		step = 0
 		while step < steps:
 			a0, a = self.probability()
-			reactnum = choosereaction(a0, a)
-			timestep = calctimestep(a0)
+			reactnum = self.choosereaction(a0, a)
+			timestep = self.calctimestep(a0)
 
 			for colony in self.reactions[reactnum].colonies:
 				if colony.name in self.reactions[reactnum].reactants:
 					colony.size -= self.reactions[reactnum].reactants[colony.name]
 				if colony.name in self.reactions[reactnum].products:
 					colony.size += self.reactions[reactnum].products[colony.name]			
+
+			for colony in self.colonies:
+				data[colony.name].append(colony.size)
 
 			step += 1
 
@@ -146,6 +158,47 @@ class gillespie:
 
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+H = colony('H', 0)
+H2 = colony('H2', 1000)
+R1 = reaction({'H': 1},{'H2': 1}, 'Binding', [H, H2], 0.5)
+R2 = reaction({'H2': 1},{'H': 1}, 'Degradation', [H, H2], 0.5)
+G = gillespie([R1, R2], [H, H2])
+data = G.run(10000)
+plt.plot(np.array(data['H']))
+plt.show()
+    
+
+progenitors = []
+peripheral = []
+reactions = []
+for clone in range(10):
+    progenitors.append(colony('progenitor'), 0)
+    peripheral.append(colony('peripheral', 0))
+    reactions.append(reaction({}, {}, 'StemDiff', [], 0.1))
+    reactions.append(reaction({}, {}, 'Renew', [], 0.1))
+    reactions.append(reaction({}, {}, 'BloodCreat', [], 0.1))
+    reactions.append(reaction({}, {}, 'ProgDeath', [], 0.1))
+    reactions.append(reaction({}, {}, 'PeriphDeath', [], 0.1))
+
+
+    C = Colony("Base", 0)
+    R1 = Reaction("Renew", [C], [C,C], p)
+    R2 = Reaction("Death", [C], [], muP)
+    R3 = Reaction("BloodCreat", [C], [], omega)
+    R4 = Reaction("StemDiff", [], [C], alpha)
+    reactions = [R1, R2, R3, R4]
+    
+    # Create colonies for the simulation
+    colonies = []
+    for ind in range(numCol):
+        numCells = 0
+        colonies.append(Colony("P{0}".format(ind), numCells))
+    
+    gillespieSim = Gillespie(reactions, colonies)
+    gillespieSim.run(timeLength)
 
 
 
